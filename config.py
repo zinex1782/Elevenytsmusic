e893a85433cb95310b498f6cc705de86a3280519
zinex1782 <musicbot1067@gmail.com>
2026-05-24 16:27:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -57,8 +57,8 @@ class Config:
 
         # ============ SUPPORT LINKS ============
         self.SUPPORT_CHANNEL: str = getenv(
-            "SUPPORT_CHANNEL", "https://t.me/elevenytsmusic")
-        self.SUPPORT_CHAT: str = getenv("SUPPORT_CHAT", "https://t.me/elevenytschats")
+            "SUPPORT_CHANNEL", "https://t.me/yukusupport")
+        self.SUPPORT_CHAT: str = getenv("SUPPORT_CHAT", "https://t.me/yukusupport")
 
         # ============ EXCLUDED CHATS ============
         # Parse comma-separated chat IDs that assistants should never leave
